44b186787b8decec7e60348754c5b890d1718076
tr1sm0s1n <mobinm.mohanan@gmail.com>
2025-10-06 22:16:04 +0530

    🌱 | add PostgreSQL seed script
    Signed-off-by: tr1sm0s1n <mobinm.mohanan@gmail.com>

---
+++ scripts/init.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS sinners;
+
+CREATE TABLE sinners (
+    id             BIGINT PRIMARY KEY,
+    "name"         VARCHAR(20) NOT NULL,
+    class          CHAR(1) NOT NULL,
+    libram         VARCHAR(20),
+    tendency       VARCHAR(20),
+    created_at     TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
+    updated_at     TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW() NOT NULL
+);
+
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER set_updated_at
+BEFORE UPDATE ON sinners
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at_column();
+
+INSERT INTO sinners (id, "name", class, libram, tendency)
+VALUES
+    (14, 'Deren', 'S', 'Fraud', 'Fury'),
+    (17, 'Shalom', 'S', 'Sloth', 'Reticle');
